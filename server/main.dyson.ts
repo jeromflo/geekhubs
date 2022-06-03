fe8c621d30e97a5e414aa96ba0007f6f61cf1f97
jeromflo <jeromflo@gmail.com>
2022-06-03 17:57:37 +0200

    estructura dyson

---
+++ server/main.dyson.ts
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        cache: false,
+        path: "/getItem",
+        method: "POST",
+        secure: false,
+        template: (params: any, query: any, body: any) => {
+
+        },
+        delay: 1000,
+    },
+]
