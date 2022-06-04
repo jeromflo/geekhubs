ecedb96c46083ab3da765e21e6bea77bf8f0a95d
jeromflo <jeromflo@gmail.com>
2022-06-04 12:56:18 +0200

    cambiado componentes a dinamicos y terminado las vistas
    terminado el componente de vision general del carrito, y main page con componente dinamico completo en groupCard

--- src/app/core/components/sections/section-cart/section-cart.component.ts
+++ src/app/core/components/sections/section-cart/section-cart.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Item } from 'src/app/shared/interfaces/item';
+import { remove } from 'src/app/shared/redux/actions/cart.actions';
 
 @Component({
   selector: 'app-section-cart',
   templateUrl: './section-cart.component.html',
   styleUrls: ['./section-cart.component.css']
 })
-export class SectionCartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SectionCartComponent {
+  items: Item[] = []
+  constructor(private router: Router, private store: Store<{ cart: Item[] }>) {
+    this.store.select('cart').subscribe(cart => {
+      this.items = cart;
+      if (this.items.length === 0) {
+        this.goBack();
+      }
+    })
   }
 
+
+  deleteElement(item: Item) {
+    this.store.dispatch(remove({ value: item.id }))
+  }
+  getCssButton() {
+    return `  <span style="color:white"><i
+                            class="fas fa-cart-arrow-down"></i></span>`
+  }
+  goBack() {
+    if (this.items.length === 0) {
+      this.router.navigate(['/mainPage']);
+    }
+  }
 }
+
--- src/app/shared/components/cards-groups/cards-groups.component.ts
+++ src/app/shared/components/cards-groups/cards-groups.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 import { Item } from '../../interfaces/item';
 
 @Component({
@@ -9,13 +9,15 @@ import { Item } from '../../interfaces/item';
 export class CardsGroupsComponent {
   @Input() items: Item[] = [];
   @Input() disabled: null | true = null;
-  @Input() textButton: string = '';
-  @Input() cssButton: string = "btn btn-primary align-self-end";
-  @Output() handlcerClick: EventEmitter<Item> = new EventEmitter<Item>();
+  /*   @Input() textButton: string = '';
+    @Input() cssButton: string = "btn btn-primary align-self-end";
+    @Output() handlcerClick: EventEmitter<Item> = new EventEmitter<Item>(); */
+  @ContentChild('button') button: TemplateRef<HTMLElement> | null = null;
+
   constructor() {
   }
-  clickMe(item: Item) {
-    this.handlcerClick.emit(item);
-  }
+  /*   clickMe(item: Item) {
+      this.handlcerClick.emit(item);
+    } */
 
 }
